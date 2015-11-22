e749a43b4517c69c68ff6498fc84384b18cf980d
Nikhil Mascarenhas <nikhil.mascarenhas@gmail.com>
2015-11-21 21:48:25 -0500

    Changed CSV to R objects. Added Data. Data still not 100%clean

--- src/extractor/ExtractMainPage.R
+++ src/extractor/ExtractMainPage.R
@@ -1,26 +1,46 @@
-#This function reads and parses the first main page in IMDB
-#INPUT:
+#########################################################################
+#
+# This file contains extractMainPage() function.
+#
+# This function reads and parses a list of all movies 1972-present on IMDB (url of the list below)
+# 
+# INPUT:
 #  - URL: http://www.imdb.com/list/ls057823854/?start=1&view=compact&sort=release_date_us:desc&defaults=1&defaults=1&release_date=1970,2015
 #
-#OUTPUT:
-#  - CSV file "mainPageData.csv" which has below columns:
+# OUTPUT:
+#  - Data Frame "mainPageData" which has below columns:
 #     - MovieID
 #     - MovieURL
 #     - MovieTitle
 #     - ReleaseYear
 #     - MovieRating
 #     - MovieNumUserRatings
+#
+###########################################################################
+
+#Install and load required libraries
+
+# install.packages("rvest")
+# install.packages("magrittr")
+# install.packages("stringr")
+# install.packages("plyr")
 
 library(rvest)
 library(magrittr)
 library(stringr)
+library(plyr)
 
-# imdb_name <- "www.imdb.com"
+############################################################################
 
+#The movie list is split into multiple pages on IMDB, each page containing 250 movies
+#The below function can process a single such page, as indicated by the startNumber
 
-extractMainPage <- function(url){
-  source.page <- read_html(url)
+extractSinglePage <- function(startNumber){
+  print(startNumber)
+  url <- paste0("http://www.imdb.com/list/ls057823854/?start=", startNumber , 
+                "&view=compact&sort=release_date_us:desc&defaults=1&defaults=1&release_date=1970,2015")
   
+  source.page <- read_html(url)
   # MovieTitle
   MovieTitle <- source.page %>%
     html_nodes(".title") %>%
@@ -36,67 +56,43 @@ extractMainPage <- function(url){
                                                 html_attr("href")), "/title/", ""), "/", "")
   # MovieID <- append("MovieID", MovieID)
   
-  # ReleaseYear
-  ReleaseYear <- source.page %>%
-    html_nodes(".year") %>%
-    html_text()
-  ReleaseYear <- ReleaseYear[2:(length(ReleaseYear) - 1)]
-  
-  # MovieRating
-  MovieRating <- source.page %>%
-    html_nodes(".user_rating") %>%
-    html_text()
-  MovieRating <- MovieRating[2:length(MovieRating)]
-  
-  # MovieNumUserRatings
-  MovieNumUserRatings <- source.page %>%
-    html_nodes(".num_votes") %>%
-    html_text()
-  MovieNumUserRatings <- MovieNumUserRatings[2:length(MovieNumUserRatings)]
-
   # Combine all into Data Frame
-  df <- data.frame(MovieTitle, MovieID, MovieURL, ReleaseYear, MovieRating, MovieNumUserRatings)
+  df <- data.frame(MovieTitle, MovieID, MovieURL)
 
   return(df)
   
 }
 
-# MainPage1 <- extractMainPage(url1)
-# MainPage2 <- extractMainPage(url2)
+############################################################################
 
-MP <- c()
-item <- c(1, 251, 501, 751, 1001, 1251, 1501, 1751, 2001, 2251, 
-          2501, 2751, 3001, 3251, 3501, 3751, 4001, 4251, 4501)
+extractMainPage <- function(){
 
-for (i in item) {
-  # Change URL to scrape multiple pages
-  url <- paste0("http://www.imdb.com/list/ls057823854/?start=", i , 
-                "&view=compact&sort=release_date_us:desc&defaults=1&defaults=1&release_date=1970,2015")
+  startNum <- ((0:38)*250)+1
   
-  # Combine scraped data frames from each page into one data frame
-  MP <- rbind(MP, extractMainPage(url))
-
+  MP_List <- lapply(startNum, extractSinglePage)
+  MP_List
+  mainPageData <- ldply(MP_List, data.frame)
+  return(mainPageData)
+  
+  #  I am keen to move this to a "data Pre processing" stage.
+  # So that we can do preprocessing of all variables in one go. - Nikhil
+  
+  #mainPageData$ReleaseYear <- as.numeric(as.character(mainPageData$ReleaseYear))
+  #MainPage$MovieRating <- as.numeric(as.character(MainPage$MovieRating))
+  #MainPage$MovieNumUserRatings <- as.character(MainPage$MovieNumUserRatings)
+  #MainPage$MovieNumUserRatings <- str_replace_all(MainPage$MovieNumUserRatings, ",", "")
+  #MainPage$MovieNumUserRatings <- str_replace_all(MainPage$MovieNumUserRatings, "-", "")
+  #MainPage$MovieNumUserRatings <- as.numeric(MainPage$MovieNumUserRatings)
+  
+  # Select only movies from years 2000 and higher
+  #MainPage <- MainPage[MainPage$ReleaseYear > 1999, ]
+  #barplot(table(mainPageData$ReleaseYear))
+  
+  # Write to CSV file:
+  #write.csv(MainPage, file = "../../data/MainPageData.csv", row.names = FALSE)
+  
+  # Save R.Data
+  #save(list = ls(all.names = TRUE), file="ExtractMainPage_11.19.2015.RData")
+  
 }
-
-MainPage <- MP
-
-
-MainPage$ReleaseYear <- as.numeric(as.character(MainPage$ReleaseYear))
-MainPage$MovieRating <- as.numeric(as.character(MainPage$MovieRating))
-MainPage$MovieNumUserRatings <- as.character(MainPage$MovieNumUserRatings)
-MainPage$MovieNumUserRatings <- str_replace_all(MainPage$MovieNumUserRatings, ",", "")
-MainPage$MovieNumUserRatings <- str_replace_all(MainPage$MovieNumUserRatings, "-", "")
-MainPage$MovieNumUserRatings <- as.numeric(MainPage$MovieNumUserRatings)
-
-# Select only movies from years 2000 and higher
-MainPage <- MainPage[MainPage$ReleaseYear > 1999, ]
-barplot(table(MainPage$ReleaseYear))
-
-# Write to CSV file:
-write.csv(MainPage, file = "../../data/MainPageData.csv", row.names = FALSE)
-
-# Save R.Data
-save(list = ls(all.names = TRUE), file="ExtractMainPage_11.19.2015.RData")
-
-
-
+############################################################################
