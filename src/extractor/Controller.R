dda6681d89d885a2c274c01e9cad174a7775c757
Matt Hawthorn <hawthorn.matthew@gmail.com>
2015-12-01 08:28:51 -0500

    Integrated other extraction tasks with the controller.

--- src/extractor/Controller.R
+++ src/extractor/Controller.R
@@ -1,9 +1,31 @@
 # Control File. Calls all other function and saves final result.
 # Before running set working directory to location of this file(Controller.R).
-source("ExtractMainPage.R")
-source("ExtractMovieDetails.R")
-mainPage <- extractMainPage()
-save(mainPage,file="../../data/mainPageData.RData")
-load(file="../../data/mainPageData.RData")
-movieDataFrame  <-  extractMovieDetails(mainPage)
-save(movieDataFrame,file="../../data/movieData.RData")
+dataDir <- "~/Git/OscarPredictor/data/"
+sourceDir <- "~/Git/OscarPredictor/src/extractor/"
+
+source(paste0(sourceDir,"ExtractMainPage.R"))
+source(paste0(sourceDir,"ExtractMovieDetails2.R"))
+source(paste0(sourceDir,"ExtractActorDetails.R"))
+
+
+if(!file.exists(paste0(dataDir,"mainPageData.RData"))){
+    mainPage <- extractMainPage()
+    save(mainPage, file=paste0(dataDir,"mainPageData.RData"))
+} else {
+    load(file=paste0(dataDir,"mainPageData.RData"))
+}
+
+if(!file.exists(paste0(dataDir,"movieData.RData"))){
+    movieDataFrame  <-  extractMovieDetails(mainPage, chunksize = 100, 
+                                            startIndex = 1, endIndex = 10,#nrow(mainPage), 
+                                            dataDir = dataDir)
+    save(movieDataFrame, file=paste0(dataDir,"movieData.RData"))
+} else {
+    load(file=paste0(dataDir,"movieData.RData"))
+}
+
+if(!file.exists(paste0(dataDir,"actorData.RData"))){
+    actorData  <-  extractActorDetails(movieDetails = movieDataFrame, chunksize = 100, 
+                                       startIndex = 1, endIndex = 10, dataDir = dataDir)
+    save(actorData, file=paste0(dataDir,"actorData.RData"))
+}
